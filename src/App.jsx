d3eb562cc27cf83460b4981237679048979574aa
ankurguha2910 <ankur.guha16@gmail.com>
2025-08-06 13:33:54 +0530

    Designing Routing using BrowserRouter from reacct-router package and creating child routes using Outlet component, NavBar and Footer added to the app from daisyUI component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router"
+import NavBar from "./NavBar"
+import Body from "./Body"
+import Login from "./Login"
+import Profile from "./Profile"
+
 function App() {
 
   return (
-    <h1>Dev Tinder</h1>
+    <>
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<Body/>}>
+          <Route path="/login" element={<Login/>}></Route>
+          <Route path="/profile" element={<Profile/>}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    {/* <NavBar />
+    <h1 className="text-xl font-bold flex justify-center my-2">Hello!</h1> */}
+    </>
   )
 }
 
